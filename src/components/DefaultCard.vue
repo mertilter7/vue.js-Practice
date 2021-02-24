ba8cbea21ecacc869774dc9343cc97f7b311400c
mertilter7 <57452662+mertilter7@users.noreply.github.com>
2021-02-25 01:38:18 +0300

    Basic Game with Vue
    vue practice

---
+++ src/components/DefaultCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="card"></div>
+</template>
+<script>
+
+</script>
+
+<style scoped>
+
+.card {
+        width: 200px;
+        height: 232px;
+        border-radius: 5px;
+        border: 1px solid #666;
+        margin-left: 10px;
+        background-image: url(../assets/vue-js-prac.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        box-shadow: 0px 9px 55px #666;
+        
+    }
+
+ 
+
+    .card img {
+        width: 100%;
+        height: 100%;
+    }
+</style>
---
+++ src/components/Failure.vue
@@ -0,0 +1,55 @@
+<template>
+   <div class="container">
+       <h1>Malesef Bilemediniz!!</h1>
+       <button @click="newGame">Yeni Oyun
+       </button>
+   </div>
+</template>
+<script>
+export default {
+    methods : {
+        newGame(){
+            this.$emit("activeComponentEvent", "app-game-cards")
+        }
+    }
+}
+</script>
+<style scoped>
+ .container {
+     position: absolute;
+     top: 0px;
+     left: 0px;
+     width: 100%;
+     height: 100%;
+     display: flex;
+     flex-direction: column;
+     align-items: center;
+     justify-content: center;
+     background-color: darkslateblue;
+}
+
+.container h1 {
+    text-align: center;
+    font-size: 333%;
+    color: #fff;
+}
+.container button {
+    cursor: pointer;
+    width: 200px;
+    background-color: #fff;
+    color: black;
+    border: 1px solid brown;
+    height: 50px;
+    font-size: 15px;
+    border-radius: 5px;
+    outline: none;
+    transition: all .5s;
+    font-weight: bold;
+}
+
+.container button:hover{
+    background-color: lightgreen;
+    color: black;
+    transition: all .5s;
+}
+</style>
